914433b70ebc07070ad32f9999151e3af3aad254
f0rsakeN-afk <nar3sh0@gmail.com>
2025-07-01 08:08:58 +0545

    websocket initialized

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import http from "http";
+import { Server } from "socket.io";
+import { registerSocketHandlers } from "./socket";
+import roomRoutes from "./routes/rooms";
+import path from "path";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${process.env.PORT}`);
+app.use(express.json());
+app.use("/rooms", roomRoutes);
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.send("Websocket running");
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected:${socket.id}`);
+  registerSocketHandlers(socket, io);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected ${socket.id}`);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
